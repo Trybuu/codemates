eb43e84a392f2463a9ab0f47b4144ba929d5c5f6
trybulamarek <trybu001@gmail.com>
2024-02-14 12:10:24 +0100

    Routes, navbar and footer

--- client/src/layouts/Root.jsx
+++ client/src/layouts/Root.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
 
 export default function Root() {
   return (
     <>
       <Navbar />
-      <Outlet />
-      <div>Footer</div>
+      <div className="content-wrapper">
+        <Outlet />
+      </div>
+      <Footer />
     </>
   )
 }
