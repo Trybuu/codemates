558eec890027156d3c306ec9c49c7a71333d7e33
trybulamarek <trybu001@gmail.com>
2024-02-27 13:38:45 +0100

    static files

--- server/app/app.js
+++ server/app/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const app = express()
 const announcementsRouter = require('./routes/announcements')
@@ -8,12 +9,14 @@ const technologiesRouter = require('./routes/technologies')
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, 'client')))
+
 app.use('/announcements', announcementsRouter)
 app.use('/auth', authRouter)
 app.use('/technologies', technologiesRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../client', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
 
 module.exports = app
