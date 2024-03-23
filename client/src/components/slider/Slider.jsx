23f1069d2862b632a007d6a9a6ee3052a03c8869
trybulamarek <trybu001@gmail.com>
2024-03-23 17:32:58 +0100

    footer

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,3 +1,13 @@
+import Testimonial from '../testimonials/Testimonial'
+import classes from './Slider.module.scss'
+
 export default function Slider() {
-  return <div>SLIDER</div>
+  return (
+    <div className={classes['slider']}>
+      <div className={classes['slider__content']}>
+        <Testimonial />
+        <Testimonial />
+      </div>
+    </div>
+  )
 }
--- client/src/components/testimonials/Testimonial.jsx
+++ client/src/components/testimonials/Testimonial.jsx
@@ -13,12 +13,16 @@ export default function Testimonial() {
         </div>
         <div className={classes['testimonial__quote']}>
           <p>
-            {' '}
-            I love codemates! I found someone with passion to programming and we
-            made a huge project together!
+            {`"Codemates is not just a place where we code - it's a community that
+            fuels our passion for programming. Thanks to Codemates, I can not
+            only create amazing projects, but also meet inspiring individuals
+            with whom I can learn and grow every day. It's more than just a
+            platform - it's an opportunity for discovery, collaboration, and
+            mutual motivation. Join Codemates and spread your wings in the world
+            of programming!"`}
           </p>
 
-          <p className={classes['testimonial__author']}>Jenny Wilson</p>
+          <p className={classes['testimonial__author']}>Elizabeth Taylor</p>
         </div>
       </div>
     </section>
